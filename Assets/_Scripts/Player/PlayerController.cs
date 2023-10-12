c3aeb4f5a1080bb7933ed9754fb381d150eb3aaf
Wayne Michael Pangilinan <113033667+Makiiidon@users.noreply.github.com>
2023-10-12 14:33:05 +0800

    Animations

--- Assets/_Scripts/Player/PlayerController.cs
+++ Assets/_Scripts/Player/PlayerController.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Animations;
 
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody2D m_rb;
     private InputManager m_inputManager;
+
+    private Animator m_anim;
 
     [SerializeField] private float m_speed = 250f;
 
@@ -14,7 +17,8 @@ public class PlayerController : MonoBehaviour
     {
         // Gets the Reference of the input manager
         m_inputManager = InputManager.instance;
-        m_rb = GetComponent<Rigidbody2D>();
+        m_rb = GetComponent<Rigidbody2D>(); 
+        m_anim = GetComponent<Animator>();
     }
 
 
@@ -34,7 +38,15 @@ public class PlayerController : MonoBehaviour
     {
         // One of many Computations for rigidbody movement
         m_rb.velocity = m_inputManager.GetMoveInput().normalized * m_speed;
-
+        
+        if (m_inputManager.GetMoveInput() == Vector2.zero)
+        {
+            m_anim.SetBool("IsMoving", false);
+        }
+        else
+        {
+            m_anim.SetBool("IsMoving", true);
+        }
         // Alternative for movement, but is more Physics based
         //m_rb.AddForce(m_inputManager.GetMoveInput().normalized * m_speed, ForceMode2D.Force);
 
