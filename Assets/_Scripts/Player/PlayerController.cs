b8c3e6fde1cd16459d7dc9a39ec77f1016ae5f91
Wayne Michael Pangilinan <113033667+Makiiidon@users.noreply.github.com>
2023-11-11 14:33:37 +0800

    Doggo

--- Assets/_Scripts/Player/PlayerController.cs
+++ Assets/_Scripts/Player/PlayerController.cs
@@ -1,7 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Tilemaps;
 using UnityEngine;
 using UnityEngine.Animations;
+using UnityEngine.Apple;
 
 public class PlayerController : MonoBehaviour
 {
@@ -11,6 +13,9 @@ public class PlayerController : MonoBehaviour
     private Animator m_anim;
 
     [SerializeField] private float m_speed = 250f;
+    //[SerializeField] private SpriteRenderer m_sprite;
+
+    bool m_isFacingLeft = true;
 
     // Start is called before the first frame update
     void Start()
@@ -47,9 +52,37 @@ public class PlayerController : MonoBehaviour
         {
             m_anim.SetBool("IsMoving", true);
         }
+
+        if (m_rb.velocity != Vector2.zero)
+        {
+            m_anim.SetBool("IsWalking", true);
+            Debug.Log("Working");
+
+        }
+        else if (m_rb.velocity == Vector2.zero)
+        {
+            m_anim.SetBool("IsWalking", false);
+        }
+
+        //// Set destination point
+        //Vector2 direction = destination - currentPos;
+
+        //if (m_isFacingLeft && direction.x > 0)
+        //{
+        //    m_anim.SetTrigger("Flip");
+
+        //}
+        //else if (!m_isFacingLeft && direction.x < 0)
+        //{
+        //    m_anim.SetTrigger("Flip");
+
+        //}
+
+
         // Alternative for movement, but is more Physics based
         //m_rb.AddForce(m_inputManager.GetMoveInput().normalized * m_speed, ForceMode2D.Force);
 
         // Explore the different ways to move rigidbodies
     }
+
 }
